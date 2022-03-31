919bb27c360d534a51fff72e9840cc0528e713d1
Ruben Blanco <rubenblancomanzano@gmail.com>
2022-03-31 22:39:16 +0200

    FilePicker web part created

---
+++ react-filepicker/src/webparts/filePickerWp/loc/mystrings.d.ts
@@ -0,0 +1,10 @@
+declare interface IFilePickerWpWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+}
+
+declare module 'FilePickerWpWebPartStrings' {
+  const strings: IFilePickerWpWebPartStrings;
+  export = strings;
+}
